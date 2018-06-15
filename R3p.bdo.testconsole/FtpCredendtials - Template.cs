ec102f84ff55b33b545b8ae5cd51f6d67b4d9d04
Clemens Larkowski <masterofscare@gmx.de>
2018-06-15 09:16:48 +0200

    - added FtpCredendtials template

---
+++ R3p.bdo.testconsole/FtpCredendtials - Template.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R3p.bdo.testconsole
+{
+    public class FtpCredendtials_Template
+    {
+        public static string FTPHost = "";
+        public static string FTPurl = "";
+        public static NetworkCredential FtpCredential = new NetworkCredential("user", "password");
+    }
+}
